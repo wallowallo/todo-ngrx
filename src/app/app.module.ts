675359d69d509993726ee0b92dabec30261c5c27
Krister <krister.torsvik@gmail.com>
2017-04-18 22:28:06 +0200

    angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//components
 import { AppComponent } from './app.component';
+
+//services
+
+//imports
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -12,9 +19,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    //services
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
