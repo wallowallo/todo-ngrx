0a12ae428d700461616308da023619740b5e3315
Krister <krister.torsvik@gmail.com>
2017-05-04 13:53:02 +0200

    testing add todo

--- src/app/log-in/log-in.component.spec.ts
+++ src/app/log-in/log-in.component.spec.ts
@@ -5,21 +5,4 @@ import { LogInComponent } from './log-in.component';
 describe('LogInComponent', () => {
   let component: LogInComponent;
   let fixture: ComponentFixture<LogInComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LogInComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LogInComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,25 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoListComponent } from './todo-list.component';
+import { todos } from '../_reducers/todos';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TodoListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create an action to add a todo', () => {
+        const state = [];
+        const todo = { title: 'write tests', description: 'angular 2 tests' }
+        const action = todos(state, { type: 'ADD_TODO', payload: todo })
+        const expected = [...state, { title: 'write tests', description: 'angular 2 tests', id: new Date().getTime(), completed: false, isEdit: false, dateAdded: new Date()}];
+        expect(action).toEqual(expected);
   });
 });
